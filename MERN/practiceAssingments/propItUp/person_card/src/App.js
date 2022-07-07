a381c2ff622e291b1d221103b91cf8773d7fe6aa
aaramirezaa <ramirez.alexander46@icloud.com>
2022-07-07 09:00:13 -0700

    react assignment

--- MERN/practiceAssingments/propItUp/person_card/src/App.js
+++ MERN/practiceAssingments/propItUp/person_card/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import PersonCard from './components/PersonCard'
+import React, { useState } from 'react';
 
 function App() {
+  // const [count, setCount] = useState(0);
   return (
     <div className="App">
       {/* <legend>App.js</legend> */}
+      {/* <button onClick={() => setCount(1)}>Click me!</button> */}
       <PersonCard  firstName="Jane" lastName="Doe" age={45} hairColor="Black"/>
       <PersonCard  firstName="John" lastName="Smith" age={88} hairColor="Brown"/>
       <PersonCard  firstName="Millard" lastName="Fillmore" age={50} hairColor="Brown"/>
--- MERN/practiceAssingments/propItUp/person_card/src/components/PersonCard.jsx
+++ MERN/practiceAssingments/propItUp/person_card/src/components/PersonCard.jsx
@@ -2,6 +2,12 @@ import React, {Component} from "react"
 
 
 class PersonCard extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            {this.props.age:} 0,
+        };
+    }
     render(){
         return(
             <div>
@@ -9,6 +15,7 @@ class PersonCard extends Component{
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
                 <h2>Age: {this.props.age}</h2>
                 <h2>Hair Color: {this.props.hairColor}</h2>
+                <button onClick={() => this.setState({ this.props.age: 1})}>Birthday button for {this.props.firstName}</button>
             </div>
         )
     }

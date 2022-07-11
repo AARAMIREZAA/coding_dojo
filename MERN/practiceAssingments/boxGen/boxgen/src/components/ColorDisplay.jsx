e4ae2bd76e411339e8fd77309c544b5c95dba8ab
aaramirezaa <ramirez.alexander46@icloud.com>
2022-07-11 15:07:09 -0700

    react changes

--- MERN/practiceAssingments/boxGen/boxgen/src/components/ColorDisplay.jsx
+++ MERN/practiceAssingments/boxGen/boxgen/src/components/ColorDisplay.jsx
@@ -11,6 +11,7 @@ const ColorDisplay = (props) => {
                 allColors.map((color, index) => {
                     return (
                         <div key={index}>
+                            {/* <p> style={{backgroundColor: color, width: "100px", height: "100px"}}</p> */}
                             {color}
                         </div>
                     )

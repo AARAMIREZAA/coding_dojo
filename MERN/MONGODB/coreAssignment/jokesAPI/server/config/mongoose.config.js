af12446f44fcbe7fc346758b3ae314b360522c3a
aaramirezaa <ramirez.alexander46@icloud.com>
2022-07-19 13:46:43 -0700

    MERN

--- MERN/MONGODB/coreAssignment/jokesAPI/server/config/mongoose.config.js
+++ MERN/MONGODB/coreAssignment/jokesAPI/server/config/mongoose.config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect('mongodb://localhost/fullmern_demo', {
+mongoose.connect('mongodb://localhost/jokes', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })

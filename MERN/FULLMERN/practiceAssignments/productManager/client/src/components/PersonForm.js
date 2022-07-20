fbc916b2520ffd65fb79a94e9398aa98a0877d98
aaramirezaa <ramirez.alexander46@icloud.com>
2022-07-20 15:04:22 -0700

    FULL MERN

--- MERN/FULLMERN/practiceAssignments/productManager/client/src/components/PersonForm.js
+++ MERN/FULLMERN/practiceAssignments/productManager/client/src/components/PersonForm.js
@@ -2,16 +2,18 @@ import React, { useState } from 'react'
 import axios from 'axios';
 export default () => {
     //keep track of what is being typed via useState hook
-    const [firstName, setFirstName] = useState(""); 
-    const [lastName, setLastName] = useState("");
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState("");
+    const [description, setDescription] = useState("");
     //handler when the form is submitted
     const onSubmitHandler = e => {
         //prevent default behavior of the submit
         e.preventDefault();
         //make a post request to create a new person
         axios.post('http://localhost:8000/api/people', {
-            firstName,
-            lastName
+            title,
+            price,
+            description
         })
             .then(res=>console.log(res))
             .catch(err=>console.log(err))
@@ -19,13 +21,18 @@ export default () => {
     //onChange to update firstName and lastName
     return (
         <form onSubmit={onSubmitHandler}>
+            <h1>Product Manager</h1>
             <p>
-                <label>First Name</label><br/>
-                <input type="text" onChange={(e)=>setFirstName(e.target.value)} value={firstName}/>
+                <label>Title</label><br/>
+                <input type="text" onChange={(e)=>setTitle(e.target.value)} value={title}/>
             </p>
             <p>
-                <label>Last Name</label><br/>
-                <input type="text" onChange={(e)=>setLastName(e.target.value)} value={lastName}/>
+                <label>Price</label><br/>
+                <input type="text" onChange={(e)=>setPrice(e.target.value)} value={price}/>
+            </p>
+            <p>
+                <label>Description</label><br/>
+                <input type="text" onChange={(e)=>setDescription(e.target.value)} value={description}/>
             </p>
             <input type="submit"/>
         </form>
